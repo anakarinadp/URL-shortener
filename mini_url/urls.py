371eafa6156ec09835f2d2eecda70f9452f3d7e1
Ana Karina De Ponte <31733847+anakarinadp@users.noreply.github.com>
2018-06-26 16:19:21 +0100

    All the functionalities implemented

--- mini_url/urls.py
+++ mini_url/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('shorten/', views.shorten, name='shorten'), # url(r'^shorten$', views.shorten, name='shorten'),
     path('', views.list_all, name='list_all'), # url(r'^$', views.list_all, name='list_all'),
-    path('redirect/<str:code>', views.redirect, name='redirect'), # url(r'^(?P<code>\w{6}/$', views.redirect, name='redirect'),
+    path('redirection/<str:code>', views.redirection, name='redirection'), # url(r'^(?P<code>\w{6}/$', views.redirection, name='redirection'),
 ]
